c084a77e76b15057c16b90e1ebfc69d0ee527af9
USUARIO <USUARIO@DESKTOP-VF07CQ6>
2022-10-16 12:05:25 -0500

    Media tarea

---
+++ src/main/java/com/mycompany/mavenproject2/Empleado.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Empleado {
+    
+    String nombre;
+    int horasTrabajadas; 
+    double costoHora;
+    int yearIngreso;
+    
+    public String mostrarInfo(){
+        var retorno = "empleado";
+        
+        return retorno;
+    }
+    
+    public double calcularIngresos(int yearActual){
+        
+        var retorno=0.0;
+      
+       
+        return retorno;
+    }
+    
+    public double calcularBonosHorasExtra(){
+        
+        var retorno=0.0;
+        
+        return retorno;
+        
+    }
+    
+    public double calcularImpuestos(int limite1, int limite2, int limite3){
+        
+        var retorno=0.0;
+        
+        return retorno;
+    }
+    
+    public double calcularTotal(){
+        
+        var retorno=0.0;
+        
+        return retorno;
+    }
+   
+}
---
+++ src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+        
+        var empleado1 = new Empleado();
+        
+        empleado1.nombre="Sebastian Armijos";
+        empleado1. horasTrabajadas= 13;
+        empleado1.costoHora=1.50;
+        empleado1.yearIngreso=250;
+        
+        var empleado2 = new Empleado();
+        
+        empleado2.nombre="Alex Lopez";
+        empleado2.horasTrabajadas=16;
+        empleado2.costoHora=1.50;
+        empleado2.yearIngreso=300;
+        
+        var empleado3 = new Empleado();
+        
+        empleado3.nombre= "Mariana Torres";
+        empleado3.horasTrabajadas= 15;
+        empleado3.costoHora=1.50;
+        empleado3.yearIngreso=400;
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+
+    
+    
+    
+}
