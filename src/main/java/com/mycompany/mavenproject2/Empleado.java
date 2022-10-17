a4aa0104dd46bb85bacbcda02e2b746f8445dbd2
USUARIO <USUARIO@DESKTOP-VF07CQ6>
2022-10-17 12:09:53 -0500

    Finalizado

--- src/main/java/com/mycompany/mavenproject2/Empleado.java
+++ src/main/java/com/mycompany/mavenproject2/Empleado.java
@@ -17,41 +17,80 @@ public class Empleado {
     
     public String mostrarInfo(){
         
-        var retorno = "El empleado se llama: " + this.nombre+ "ha trabajado "
-                + this.horasTrabajadas + "cobrando por hora " + 
-                "generando al año un ingrso de: " + this.yearIngreso;
+        var retorno = "El empleado se llama: " + this.nombre+ " ha trabajado "
+                + this.horasTrabajadas + " horas, "+ "cobrando " +
+                this.costoHora+ " por hora " + "generando al año un ingreso de "
+                + this.yearIngreso + " dolares";
         
         return retorno;
     }
     
     public double calcularIngresos(int yearActual){
         
+      var antiguedad=0.0d;
         var retorno=0.0d;
-      
-       
-        return retorno;
+        var porcentaje=0.0d;
+        var horaExtra=0;
+        if (this.horasTrabajadas<=160){
+            porcentaje=(((yearActual)-this.yearIngreso)*2)/100;
+            antiguedad=(this.costoHora*this.horasTrabajadas*porcentaje);
+            retorno=(this.costoHora*this.horasTrabajadas)+antiguedad;
+            return retorno;
+        }else{
+            porcentaje=(((yearActual)-this.yearIngreso)*2)/100;
+            horaExtra=this.horasTrabajadas-160;
+            antiguedad=(this.costoHora*(this.horasTrabajadas-horaExtra)*porcentaje);
+            retorno=(this.costoHora*(this.horasTrabajadas-horaExtra))+antiguedad;
+            return retorno;
+        }
     }
+     
     
     public double calcularBonosHorasExtra(){
         
-        var retorno=0.0;
-        
-        return retorno;
-        
+     var retorno=0.0d;
+        if(this.horasTrabajadas>160){
+            var horaExtra=0;
+            horaExtra=(this.horasTrabajadas-160)*2;
+            retorno=this.costoHora*horaExtra;
+            return retorno;
+        }else{
+            retorno=0;
+            return retorno;
+        }
+       
     }
     
     public double calcularImpuestos(int limite1, int limite2, int limite3){
         
-        var retorno=0.0;
+        var retorno=0.0d;
+        var basico = this.costoHora*this.horasTrabajadas;
+        if (basico>limite1 && basico< limite2) {
+            retorno = basico*0.05;
+        }
+        else{
+            if (basico> limite2 && basico < limite3){
+                retorno= basico*0.12;
+            }
+        else {
+                if (basico>limite3){
+                retorno=basico*0.25;
+               
+                }
+             }
+                
+        }
         
         return retorno;
     }
     
-    public double calcularTotal(){
+     public double calcularTotal(int yearActual,int limite1,int limite2,int limite3 ){
+        var retorno= 0.0d;
+        retorno=this.calcularIngresos(yearActual)+this.calcularBonosHorasExtra()
+                -this.calcularImpuestos(limite1, limite2, limite3);
         
-        var retorno=0.0;
-        
-        return retorno;
+        return retorno;         
     }
+    
    
 }
--- src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
+++ src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -17,24 +17,56 @@ public class Mavenproject2 {
         var empleado1 = new Empleado();
         
         empleado1.nombre="Sebastian Armijos";
-        empleado1. horasTrabajadas= 13;
-        empleado1.costoHora=1.50;
-        empleado1.yearIngreso=250;
+        empleado1. horasTrabajadas= 150;
+        empleado1.costoHora=5.50;
+        empleado1.yearIngreso=2011;
+        
+        System.out.println(empleado1.mostrarInfo());
+        System.out.println("El empleado gana un total de: "
+                + ""+empleado1.calcularIngresos(2022)+" $");
+        System.out.println("El empleado gana un total de horas extras de: "
+                +empleado1.calcularBonosHorasExtra()+" $");
+        System.out.println("El empleado paga de impusto: "
+             +empleado1.calcularImpuestos(1000, 2000, 3000));
+        System.out.println("El empleado gana un total de: "
+                +empleado1.calcularTotal
+        (2022, 1000, 2000, 3000)+" $");
         
         var empleado2 = new Empleado();
         
         empleado2.nombre="Alex Lopez";
-        empleado2.horasTrabajadas=16;
-        empleado2.costoHora=1.50;
-        empleado2.yearIngreso=300;
+        empleado2.horasTrabajadas=170;
+        empleado2.costoHora=5.50;
+        empleado2.yearIngreso=500;
+        
+        System.out.println(empleado2.mostrarInfo());
+        System.out.println("El empleado gana un total de: "
+                +empleado2.calcularIngresos(2022)+" $");
+        System.out.println("El empleado gana un total de horas extras de: "
+                +empleado2.calcularBonosHorasExtra()+" $");
+        System.out.println("El empleado paga de impusto: "
+             +empleado2.calcularImpuestos(1000, 2000, 3000));
+        System.out.println("El empleado gana un total de: "
+                +empleado2.calcularTotal
+        (2022, 1000, 2000, 3000)+" $");
         
         var empleado3 = new Empleado();
         
         empleado3.nombre= "Mariana Torres";
-        empleado3.horasTrabajadas= 15;
-        empleado3.costoHora=1.50;
+        empleado3.horasTrabajadas= 160;
+        empleado3.costoHora=5.50;
         empleado3.yearIngreso=400;
         
+        System.out.println(empleado3.mostrarInfo());
+         System.out.println("El empleado gana un total de: "
+                 +empleado3.calcularIngresos(2022)+" $");
+        System.out.println("El empleado gana un total de horas extras de: "
+                +empleado3.calcularBonosHorasExtra()+" $");
+        System.out.println("El empleado paga de impusto: "
+             +empleado3.calcularImpuestos(1000, 2000, 3000));
+        System.out.println("El empleado gana un total de: "
+                +empleado3.calcularTotal
+        (2022, 1000, 2000, 3000)+" $");
         
         
         
